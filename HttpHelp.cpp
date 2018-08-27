a60860fd90d244b9c20e40c4d09cd5cccf87dbf8
liht10 <liht10@LIHT10-PF16RTKF>
2018-08-27 08:54:41 +0800

    增加 开源代码 mongoose

--- HttpHelp.cpp
+++ HttpHelp.cpp
@@ -2,11 +2,17 @@
 #include "HttpHelp.h"
 
 
-CHttpHelp::CHttpHelp()
-{
-}
 
 
-CHttpHelp::~CHttpHelp()
-{
+static void ev_handler(struct mg_connection *c, int ev, void *p) {
+	if (ev == MG_EV_HTTP_REPLY) {
+		c->flags |= MG_F_CLOSE_IMMEDIATELY;
+		struct http_message *hm = (struct http_message *) p;
+		fwrite(hm->message.p, 1, hm->message.len, stdout);
+		putchar('\n');
+		exit_flag = 1;
+	}
+	else if (ev == MG_EV_CLOSE) {
+		exit_flag = 1;
+	};
 }
--- HttpHelp.h
+++ HttpHelp.h
@@ -1,8 +1,8 @@
 #pragma once
-class CHttpHelp
-{
-public:
-	CHttpHelp();
-	~CHttpHelp();
-};
 
+#include "mongoose.h"
+
+static const char *url = "http://www.google.com";
+static int exit_flag = 0;
+
+static void ev_handler(struct mg_connection *c, int ev, void *p);
